02b9f91a7fe909785111da749d828c6a8baa47de
Paxton Bigler <paxton.bigler@gmail.com>
2017-02-15 07:34:31 -0700

    finish task #3

--- index_3.php
+++ index_3.php
@@ -7,8 +7,58 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,400,700" rel="stylesheet">
   </head>
   <body>
-    <!--CREATE A FUNCTION TO SEND DATA TO A MYSQL TABLE  -->
 
+    <!-- MYSQL QUERY TO CREATE NEW TABLE:
+      CREATE TABLE servers (id INT(10), server_id VARCHAR(20), server_name VARCHAR(20),
+      client_id VARCHAR(10), available INT(1), datacenter VARCHAR(30), ip_address VARCHAR(30),
+      price VARCHAR(10), server_description VARCHAR(70)); -->
+
+    <?php
+    $configs = include('config.php');
+    require './vendor/autoload.php';
+    use GuzzleHttp\Client;
+
+    $client = new Client([
+      'base_uri' => 'https://devassess.100tb.com/',
+      'timeout' => 5.0
+    ]);
+
+    $response = $client->get('testServers');
+    $data = $response->getBody();
+    $data = json_decode($data, true);
+
+    $con = mysqli_connect($configs->host,$configs->username,$configs->pass,$configs->db);
+
+    if (!$con)
+    {
+      die('Could not connect: ' . mysql_error());
+    }else{
+      echo "Connected!!";
+    }
+
+    foreach($data as $datum){
+      $id = $datum['id'];
+      $s_id = $datum['server_id'];
+      $s_name = $datum['server_name'];
+      $c = $datum['client_id'];
+      $a = $datum['available'];
+      $d = $datum['datacenter'];
+      $ip = $datum['ip_address'];
+      $p = $datum['price'];
+      $sd = $server_description['server_description'];
+
+      $sql = "INSERT INTO servers (id,server_id,server_name,
+        client_id,available,datacenter,ip_address,price,server_description)
+        VALUES ('$id','$s_id','$s_name','$c','$a','$d','$ip','$p','$sd')";
+
+      mysqli_query($con, $sql);
+    }
+
+      echo "<h2>Data Submitted!</h2>";
+
+      mysqli_close($con);
+
+    ?>
 
   </body>
 </html>
--- testing.php
+++ testing.php
@@ -194,3 +194,164 @@
 
   </body>
 </html>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>100TB Skills Assessment #2</title>
+    <link rel="stylesheet" href="/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,400,700" rel="stylesheet">
+  </head>
+  <body>
+    <!--CREATE A FUNCTION TO SEND DATA TO A MYSQL TABLE  -->
+    <?php
+
+    require './vendor/autoload.php';
+    use GuzzleHttp\Client;
+
+    $client = new Client([
+      'base_uri' => 'https://devassess.100tb.com/',
+      'timeout' => 10.0
+    ]);
+
+    $response = $client->get('testServers');
+    $data = $response->getBody();
+    $data = json_decode($data, true);
+
+    $con = mysqli_connect("127.0.0.1","root","MyNewPass","100tb");
+
+    if (!$con)
+    {
+      die('Could not connect: ' . mysql_error());
+    }else{
+      echo "Connected!!";
+    }
+
+    foreach($data as $datum){
+      $id = $datum['id'];
+      $s_id = $datum['server_id'];
+      $s_name = $datum['server_name'];
+      $c = $datum['client_id'];
+      $a = $datum['available'];
+      $d = $datum['datacenter'];
+      $ip = $datum['ip_address'];
+      $p = $datum['price'];
+      $sd = $server_description['server_description'];
+
+      var_dump($con);
+
+      // $result = mysqli_query($con,"INSERT INTO servers (id,server_id,server_name,
+      //   client_id,available,datacenter,ip_address,price,server_description)
+      //   VALUES ('$id','$s_id','$s_name','$c','$a','$d','$ip','$p','$sd')");
+      // var_dump($result);
+
+      $sql = "INSERT INTO servers (id,server_id,server_name,
+        client_id,available,datacenter,ip_address,price,server_description)
+        VALUES ('$id','$s_id','$s_name','$c','$a','$d','$ip','$p','$sd')";
+
+      mysqli_query($con, $sql);
+
+      // echo "DEBUG SQL: $sql <hr>\n";
+
+    }
+
+
+
+
+    // for ($i = 0; $i < count($data); $i++){
+    //   $id = $data[$i]->id;
+    //   $s_id = $data[$i]->server_id;
+    //   $s_name = $data[$i]->server_name;
+    //   $c = $data[$i]->client_id;
+    //   $a = $data[$i]->available;
+    //   $d = $data[$i]->datacenter;
+    //   $ip = $data[$i]->ip_address;
+    //   $p = $data[$i]->price;
+    //   $sd = $data[$i]->server_description;
+    //   mysqli_query($con, "INSERT INTO servers (id,server_id,server_name,
+    //     client_id,available,datacenter,ip_address,price,server_description)
+    //     VALUES ('{$data[$i]->id}', '{$data[$i]->server_id}', '{$data[$i]->server_name}', '{$data[$i]->client_id}', '{$data[$i]->available}',
+    //        '{$data[$i]->datacenter}', '{$data[$i]->ip_address}', '{$data[$i]->price}', '{$data[$i]->server_description}')");
+    //   mysqli_query($con, "INSERT INTO servers (id,server_id,server_name,
+    //     client_id,available,datacenter,ip_address,price,server_description)
+    //     VALUES ('{$id}','{$s_id}','{$s_name}','{$c}','{$a}','{$d}','{$ip}','{$p}','${sd}')");
+    // }
+
+
+    // for ($i = 0; $i < count($data); $i++){
+    //   $sql = mysqli_query($con, "INSERT INTO servers (id,server_id,server_name,
+    //     client_id,available,datacenter,ip_address,price,server_description)
+    //     VALUES ('{$data[$i]->id}', '{$data[$i]->server_id}', '{$data[$i]->server_name}',
+    //        '{$data[$i]->client_id}', '{$data[$i]->available}', '{$data[$i]->datacenter}',
+    //        '{$data[$i]->ip_address}', '{$data[$i]->price}', '{$data[$i]->server_description}')");
+    //    mysqli_query($con, $sql);
+    //   // var_dump($result);
+    //   echo "DEBUG SQL: $sql <hr>\n";
+    // }
+
+    //
+    // mysqli_query($con, "INSERT INTO servers (id,server_id,server_name,
+    //   client_id,available,datacenter,ip_address,price,server_description)
+    //   VALUES ('$id', '$server_id', '$server_name', '$client_id', '$available',
+    //   '$datacenter', '$ip_address', '$price', '$server_description')");
+
+    // foreach($data as $datum){
+    //   $query1 = "INSERT INTO servers (id,server_id,server_name,client_id,available,datacenter,ip_address,price,server_description)
+    //     VALUES (
+    //             '". $datum['id']."',
+    //             '". $datum['server_id']."',
+    //             '". $datum['server_ name']."',
+    //             '". $datum['client_id']."',
+    //             '". $datum['available']."',
+    //             '". $datum['datacenter']."',
+    //             '". $datum['ip_address']."',
+    //             '". $datum['price']."',
+    //             '". $datum['server_description']."'
+    //             )";
+    //
+    //   $q = mysqli_query($con, $query1) or die (mysqli_error($con));
+    // }
+
+    // MYSQL QUERY TO CREATE NEW TABLE:
+      // CREATE TABLE servers (id INT(10), server_id VARCHAR(20), server_name VARCHAR(20),
+    	// client_id VARCHAR(10), available INT(1), datacenter VARCHAR(30), ip_address VARCHAR(30),
+    	// price VARCHAR(10), server_description VARCHAR(70));
+
+
+      // mysql_select_db("100tb", $con);
+
+      // $id =  $_POST['id'];
+      // $server_id = $_POST['server_id'];
+      // $server_name = $_POST['server_name'];
+      // $client_id = $_POST['client_id'];
+      // $available = $_POST['available'];
+      // $datacenter = $_POST['datacenter'];
+      // $ip_address = $_POST['ip_address'];
+      // $price = $_POST['price'];
+      // $server_description = $_POST['server_description'];
+
+      // echo $id;
+      //
+
+
+        $test = $server_name[0];
+
+        // mysqli_query($con, "INSERT INTO servers (id,server_id,server_name,
+        //   client_id,available,datacenter,ip_address,price,server_description)
+        //   VALUES ('201', '1', 'john', '8789', '0',
+        //   'test', 'test', '45', 'test')");
+
+        // mysqli_query($con, "INSERT INTO servers (id,server_id,server_name,
+        //   client_id,available,datacenter,ip_address,price,server_description)
+        //   VALUES ('201', '1', '$data[0]['server_name']', '8789', '0',
+        //   'test', 'test', '45', 'test')");
+
+      echo "<h2>Data Submitted!</h2>";
+
+      // mysqli_close($con);
+
+    ?>
+
+  </body>
+</html>
